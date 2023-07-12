4b6f65c96a7e46f04c682fda4af3f1c7ec0b9291
Mahdi Shafiee <mahdi21.shf@gmail.com>
2023-07-12 18:44:43 +0200

    add capFirstLetter function & playerSelection

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function computerPlay() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 const computerSelection = computerPlay();
+const humanPlay = prompt(
+`What is your choice? 
+Rock / Paper / Scissors`
+)
+function capFirstLetter(string){
+    if (humanPlay !== null) {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    }
+}
+const playerSelection = capFirstLetter(humanPlay);
