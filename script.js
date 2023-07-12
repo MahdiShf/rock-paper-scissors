ea993ec01302fdfac98ad01d758191356f6b8ad9
Mahdi Shafiee <mahdi21.shf@gmail.com>
2023-07-12 19:52:51 +0200

    add singleRound function

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ function capFirstLetter(string){
     }
 }
 const playerSelection = capFirstLetter(humanPlay);
+function singleRound(playerSelection, computerSelection) {
+    if (
+      (playerSelection === "Rock" && computerSelection === "Scissors") ||
+      (playerSelection === "Scissors" && computerSelection === "Paper") ||
+      (playerSelection === "Paper" && computerSelection === "Rock")
+    ) {
+      ++playerScore;
+      alert(`You won this round,
+  Computer played ${computerSelection}.
+  ${playerSelection} beats ${computerSelection}!
+  You: ${playerScore} Computer: ${computerScore}`);
+    } else if (
+      (playerSelection === "Scissors" && computerSelection === "Rock") ||
+      (playerSelection === "Paper" && computerSelection === "Scissors") ||
+      (playerSelection === "Rock" && computerSelection === "Paper")
+    ) {
+      ++computerScore;
+      alert(`You lost this round,
+    Computer played ${computerSelection}.
+    ${computerSelection} beats ${playerSelection}!
+    You: ${playerScore} Computer: ${computerScore}`);
+    } else if (
+      (playerSelection === "Rock" && computerSelection === "Rock") ||
+      (playerSelection === "Scissors" && computerSelection === "Scissors") ||
+      (playerSelection === "Paper" && computerSelection === "Paper")
+    ) {
+      alert(`You both picked the same option, try again!
+      You: ${playerScore} Computer: ${computerScore}`);
+    } else if (humanPlay == null || humanPlay == "") {
+      alert(`Canceled!`);
+    } else {
+      alert("Wrong word.");
+    }
+  }
+  singleRound(playerSelection, computerSelection);
