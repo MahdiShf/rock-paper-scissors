8e8d416182125694d8fe18efc95dcc92042c7299
Mahdi Shafiee <mahdi21.shf@gmail.com>
2023-07-13 17:21:19 +0200

    add comments to describe functions

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let playerScore = 0;
 let computerScore = 0;
 function playGame() {
-  for (let i = 0; i < 5; i++) {
+  for (
+    let i = 0;
+    i < 5;
+    i++ // Runs the game for 5 times
+  ) {
+    // This function will randomly return either Rock, Paper or Scissors:
     function computerPlay() {
       let choices = ["Rock", "Paper", "Scissors"];
       return choices[Math.floor(Math.random() * choices.length)];
     }
     const computerSelection = computerPlay();
+    // This method will get input from the player:
     const humanPlay = prompt(
       `What is your choice? 
 Rock / Paper / Scissors`
     );
+    // This function will make the first character uppercase and convert rest of the string to lowercase:
     function capFirstLetter(string) {
       if (humanPlay !== null) {
         return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
       }
     }
     const playerSelection = capFirstLetter(humanPlay);
+    // This function will show results of round:
     function singleRound(playerSelection, computerSelection) {
       if (
         (playerSelection === "Rock" && computerSelection === "Scissors") ||
@@ -47,6 +55,7 @@ Rock / Paper / Scissors`
       You: ${playerScore} Computer: ${computerScore}`);
       } else if (humanPlay == null || humanPlay == "") {
         alert(`Canceled!`);
+        i = 5;
       } else {
         alert("Wrong word.");
       }
@@ -62,6 +71,6 @@ Rock / Paper / Scissors`
   } else if ((i = 5 && playerScore === computerScore)) {
     alert(`Game over,Tie!
     You: ${playerScore} Computer: ${computerScore}`);
-  }
+  } // Shows final results
 }
 playGame();
